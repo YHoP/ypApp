525ed8b83429cebabc4ce277b332c338171b3cd2
YHop <ypeng.tw@gmail.com>
2015-11-07 07:16:45 -0800

    rebuild gradle

--- app/src/main/java/com/epicodus/ypapp/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/ypapp/ui/MainActivity.java
@@ -13,7 +13,6 @@ import android.widget.Toast;
 
 import com.epicodus.ypapp.R;
 import com.parse.ParseAnonymousUtils;
-import com.parse.ParseObject;
 import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
@@ -36,9 +35,9 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             } else {
                 Toast.makeText(this, currentUser.getUsername(), Toast.LENGTH_LONG).show();
-                ParseObject testObject = new ParseObject("Routes");
-                testObject.put("name", "Eagle Creek run");
-                testObject.saveInBackground();
+//                ParseObject testObject = new ParseObject("Routes");
+//                testObject.put("name", "Eagle Creek run");
+//                testObject.saveInBackground();
             }
         }
 
@@ -68,16 +67,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()){
+            case  R.id.action_maps:
+                Intent mapIntent = new Intent(this, MapsActivity.class);
+                startActivity(mapIntent);
+                break;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
